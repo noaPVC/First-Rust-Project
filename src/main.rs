7e803ea011d174fbd62e7a9f93e436683245dff6
Noa Perkovic <n04.perkovic@gmail.com>
2025-04-13 17:21:15 +0200

    added project workload, tests, demo main entrypoint - fun

---
+++ src/main.rs
@@ -0,0 +1,5 @@
+
+fn main() {
+    let res = first_app::add(12, 12);
+    println!("Result: {}", res);
+}
---
+++ tests/test.rs
@@ -0,0 +1,11 @@
+use first_app::add;
+
+#[test]
+fn test_addition() {
+    assert_eq!(add(2, 2), 4);
+}
+
+#[test]
+fn test_addition_negative() {
+    assert_ne!(add(2, 2), 4);
+}
